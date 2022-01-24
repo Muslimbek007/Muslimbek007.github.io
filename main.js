4608c2ebba4aff176e4ca5d96385984563a61eaf
Muslimbek <muslimbekforce98@gmail.com>
2022-01-25 00:35:20 +0500

    all things add

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var elNav = document.querySelector('.nav');
 var elbody = document.querySelector('body');
+var elTop = document.getElementById('top');
 
 document.addEventListener('scroll', function(){
     if(window.scrollY > 220){
@@ -10,6 +11,19 @@ document.addEventListener('scroll', function(){
         elNav.classList.remove('nav--fixed');
         elbody.style.marginTop = 0;
     }
+    if(window.scrollY > 320){
+        elTop.classList.add('top-fixed');
+    }
+    else{
+        elTop.classList.remove('top-fixed');
+    }
 })
 
+let map;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
